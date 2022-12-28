b2c2aa27df25468ae4a79444f9651902204e0c0b
Jonah <jghebremichael95@gmail.com>
2022-12-28 10:50:08 -0500

    done

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace OperatorExercise
+﻿using System;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            Console.WriteLine($"{a}/{b} is " + a/b + " remainder " + a%b);
+
+            Console.WriteLine("What is the radius of your circle");
+
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {AreaOfCircle(radius)}");
+
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
         }
     }
 }
